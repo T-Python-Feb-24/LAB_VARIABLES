78191671ace2a83763e2c9c79a44c129dca69558
Shekah <shekhahathlol@gmail.com>
2024-02-06 19:27:22 +0300

    Printed the variables values

--- Lab1.py
+++ Lab1.py
@@ -2,4 +2,10 @@ footballTeam : str = "None"
 x :int = 10
 y :float = 10.0
 A = B = C = 'Letter' 
-one ,two, three = 1,2,3
+one,two,three = 1,2,3
+
+print(footballTeam)
+print(f"x:{x}, y:{y}")
+print (f"A:{A}, B:{B}, C:{C}")
+print(f"one:{one}, two:{two}, three: {three}")
+
