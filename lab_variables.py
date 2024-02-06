776ff106346ccac7dde2ff7c1688f7bdba8b7c3a
AsmaaFelemban <Asmaakf@hotmail.com>
2024-02-06 13:41:37 +0300

    solved the lab

---
+++ lab_variables.py
@@ -0,0 +1,14 @@
+#Define a variable... favourite football team.
+favourite_football_team = "Real Madrid"
+
+#Define a variable “x“  has an integer value 
+x:int = 23
+#and a variable “y” has a float value.
+y = 3.5
+
+#Assign One Value to Multiple Variables 
+val1 = val2 = val3 = 8
+
+#Assign multiple Values to Multiple Variables 
+num1, num2, num3 = 1, 2.2, "asmaa"
+
