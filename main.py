d225b684ae07c476c0335e9dccd4cd9acb1ba883
Maha <mahaalqarni.offficial@gmail.com>
2024-02-06 14:54:48 +0300

    Done

---
+++ main.py
@@ -0,0 +1,13 @@
+# Define a variable that has a value of your favourite football team.
+
+favorite_team = "Alnassr"
+
+# Define a variable “x“  has an integer value and a variable “y” has a float value.
+x = 1
+y = 2.5
+
+# Assign One Value to Multiple Variables (choose random names & values).
+num1 = num2 = num3 = 200
+
+# Assign multiple Values to Multiple Variables (choose random names & values).
+txt1, txt2, txt3 = "Maha" , "Asma"
