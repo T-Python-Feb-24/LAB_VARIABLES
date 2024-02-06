6df6fcbad7e5ea34f97c032f9b6718566ab856cb
Abdullah <.alhumidi.Ab@gmail.com>
2024-02-06 13:52:51 -0800

    Addding LAB_VARIABLES

---
+++ Lab_Variables.py
@@ -0,0 +1,10 @@
+#point number 1 Define a variable that has a value of your favourite football team.
+Fav_FT="Al Hilal"
+#point number 2 Define a variable “x“ has an integer value and a variable “y” has a float value.
+x = 10
+y = 10.5
+#point number 3 Assign One Value to Multiple Variables (choose random names & values).
+A=B=C=D=21
+#point number 4 Assign multiple Values to Multiple Variables (choose random names & values).
+Name,Major,Study_Years="Abdullah","Computer Scinces",5
+#######################################
