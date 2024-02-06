5358736a75ac8f4f00edf0429e14a5348a31e06a
Nawal <Nawalir.16@gmail.com>
2024-02-06 13:24:54 +0300

    Solve the LAB

---
+++ Var_Lab.py
@@ -0,0 +1,16 @@
+# Define a variable that has a value of your favourite football team.
+Football_Team = "Hilal"
+print(Football_Team)
+
+# Define a variable “x“ has an integer value and a variable “y” has a float value.
+x = 40
+y = 3.14
+print(x, y)
+
+# Assign One Value to Multiple Variables (choose random names & values).
+num1 = val1 = cal1 = 35
+print(num1,val1,cal1)
+
+# Assign multiple Values to Multiple Variables (choose random names & values).
+num1, val1, cal1 = 10, 20, 30
+print(num1, val1, cal1)
