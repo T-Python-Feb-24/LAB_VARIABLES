20bf74dde7f656b71f46981fbb261ca994cede4c
lama <lamayehya2000@gmail.com>
2024-02-06 14:57:37 +0300

    new lab1

---
+++ la.py
@@ -0,0 +1,10 @@
+MyFavoriateTeam:str = "Alahli"
+x:int = 23
+y:float = 23.5
+lama=yahya=fatemah= 44
+lora,siraj,nawaf = 12,13,14
+print(MyFavoriateTeam)
+print(x)
+print(y)
+print(lama)
+print(siraj)
