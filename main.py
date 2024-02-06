7a488325706a7f92824f506fb4967cf5328902dc
waad <waad.f.alanzi@gmail.com>
2024-02-06 13:38:15 +0300

    lab solution

---
+++ main.py
@@ -0,0 +1,10 @@
+
+footballTeam:str = "Chelsea"
+x : int = 90
+y :float = 3.9
+
+num1 = num2 = num3 = "Wassan" 
+
+s1 , s2 , s3 = "Waad" , "Wijdan" , "Hala"
+
+print(footballTeam , x , y , num1 , num2 , s2 , s3)
