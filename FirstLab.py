e8ea231552e480dcdfdf52f9701880f97fd312ea
Abdulaziz <benmrwan87@gmail.com>
2024-02-06 13:41:26 +0300

    try my code

---
+++ FirstLab.py
@@ -0,0 +1,16 @@
+#Define a variable that has a value of your favourite football team.
+
+MyFavoriteFootballTeam = "Al_ahli"
+
+#Define a variable “x“ has an integer value and a variable “y” has a float value.
+
+x = 87
+y = 6.8
+
+#Assign One Value to Multiple Variables (choose random names & values).
+
+Ahmed = Naif = Faisal = 10
+
+#Assign multiple Values to Multiple Variables (choose random names & values).
+
+Ali = Salman = 90, 10
