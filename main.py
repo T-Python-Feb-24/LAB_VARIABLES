322f2af153f818b0226bdd7be4ab6e087cb31f8c
Salemhu <salem.hussain.alsalem@gmail.com>
2024-02-06 14:49:38 +0300

    made it more understandable

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+#- Define a variable that has a value of your favourite football team.
 fav_team:str = 'barcelona'
+print(fav_team)
+
+#Define a variable “x“  has an integer value and a variable “y” has a float value.
 x:int = 10
+print(x)
 y:float = 4.33
+print(y)
+
+#Assign One Value to Multiple Variables (choose random names & values).
 best = goat = legend = 'messi'
+print(best)
+print(goat)
+print(legend)
+
+#Assign multiple Values to Multiple Variables (choose random names & values).
 team1, team2 = 'barcelona' , 'real madrid'
+print(team1)
+print(team2)
