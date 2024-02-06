2a16318e2085ab0e0cd29150a51651cdd362b880
WissamZaidi <wzaidi@stu.kau.edu.sa>
2024-02-06 13:29:45 +0300

    Done

---
+++ main.py
@@ -0,0 +1,14 @@
+# Define a variable that has a value of your favourite football team
+fav_football_Team: str = "Ahli"
+
+# Define a variable “x“ has an integer value and a variable “y” has a float value.
+x: int = 5
+y: float = 3.5
+
+# Assign One Value to Multiple Variables (choose random names & values).
+var1, var2, var3, var4, var5, var6 = "Wissam"
+print(var1, var2, var3, var4, var5)
+
+# Assign multiple Values to Multiple Variables (choose random names & values).
+num1, num2, num3, num4, num5, num6 = 1, 2, 3, 4, 5, 6
+print(num1, num2, num3, num4, num5, num6)
