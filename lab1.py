47def065aeb157fba16d7069a90809ddc2af09d9
Mohammed Alyahya <modezdf@gmail.com>
2024-02-06 13:38:16 +0300

    solaved the lab

---
+++ lab1.py
@@ -0,0 +1,8 @@
+#Define a variable that has a value of your favourite football team.
+My_team = "Alhilal"
+#Define a variable “x“  has an integer value and a variable “y” has a float value.
+x, y = 90, 80.5
+#Assign One Value to Multiple Variables (choose random names & values).
+var1 = var2 = var3 = "mohammed"
+#Assign multiple Values to Multiple Variables (choose random names & values).
+var1, var2 = "Mohammed", "Abdullah"
